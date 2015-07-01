bc6a4a7dfd5abc81283ce563f5ef526f5b4d1c31
Wang Jiadong <wangjiadong1993@gmail.com>
2015-07-01 13:04:57 +0800

    finished bnn detection

--- bp_test_261.m
+++ bp_test_261.m
@@ -1,9 +1,27 @@
-function bp_test_261(tm, bp, dir, name)
+function ann = bp_test_261(tm, bp, dir)
     
-    [endTime, dataStamp] = wfdbtime(dir, length(bp));
+%     [endTime, dataStamp] = wfdbtime(dir, length(bp));
     
-    wabp([dir,'/',name],[],endTime{1},[],2);
+    wabp(dir,[],[],[],2);
     
-    [ann]=rdann(name,'wabp');
+    [ann]=rdann(dir,'wabp');
+    
+%     tm_high = zeros(1, length(tm));
+    
+%     tm_high(ann) = bp(ann);
+    
+%     subplot(1,1,1);
+    
+%     hold off;
+%     
+%     plot(tm(1:500), bp(1:500));
+%     
+%     hold on;
+%     
+%     stem(tm(1:500), tm_high(1:500), 'ro');
+%     
+%     hold off;
+    
+%     subplot(2,1,2)
     
 end
--- main_func_jd.m
+++ main_func_jd.m
@@ -12,22 +12,54 @@ function y = main_func_jd(dir)
     
 %  !!!!!!   this line must be executed if no annotation file exists
 
-%     ecg_filtering_total(ecg, tm, 'ecg_filtered_261');
+    ecg_filtering_total(ecg, tm, 'ecg_filtered_261');
     
 %     read the annotation file
     ann = qrs_detect_261('ecg_filtered_261')';
+    
+    ann_back = ann;
+    
+    ann_back(end) = [];
+    
+    ann_dist = ann - [0, ann_back];
+    
+    
     
 %     read the bp 
     bp = signal(2, :);
     
 %     get the 30 and 70 percentile
-    percentiles = get_percentile_from_ecg_rr_261(ann, tm(2) - tm(1), 1, 99);
+%     percentiles = get_percentile_from_ecg_rr_261(ann, tm(2) - tm(1), 1, 99);
+% 
+%     disp(percentiles)
+    
+%     bp_filter_261(bp, percentiles, 5, get_frequency_261(tm));
+    
+    
 
-    disp(percentiles)
     
-    bp_filter_261(bp, percentiles, 5, get_frequency_261(tm));
+    %%%%%%%%%%%%%%%%%%PRINTING%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    bp_test_261(tm, bp);
+    bnn = bp_test_261(tm, bp, dir)';
+    
+    bnn_back = bnn;
+    
+    bnn_back(end) = [];
+    
+    bnn_dist = bnn - [0, bnn_back];
+    
+    output = [mean(ann_dist), std(ann_dist), prctile(ann_dist,10),...
+              prctile(ann_dist,90); mean(bnn_dist), std(bnn_dist),...
+              prctile(bnn_dist,10),prctile(bnn_dist,90)];
+    
+    disp(output);
+    
+    
+    
+    
+    
+    
+    
     
 %     plot(bp);
 %     subplot(2, 1, 1);
